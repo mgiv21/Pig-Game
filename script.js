3ff13b8c1ed11431a1982ac623d74a302c88ac21
Jose Antonio González Santos <pretsikaru@gmail.com>
2023-03-10 20:23:06 +0100

    Creando variables globales :)

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const elCurrentScores = document.querySelectorAll('.current-score');
+
+const elScores = document.querySelectorAll('.score');
+const elScore0 = document.querySelector('#score--0');
+const elScore1 = document.querySelector('#score--1');
+
+const elDice = document.querySelector('.dice');
+
+const btns = document.querySelectorAll('.btn');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
