20267c4540c61fd459388ffb8e45dd7d30925207
Manuel Izquierdo <manuelizquierdo2011@gmail.com>
2023-03-28 20:52:07 +0200

    creating changeStylePlayerActive

--- script.js
+++ script.js
@@ -1,95 +1,107 @@
-'use strict'
-
-
-
+"use strict";
 const playersScore = [0, 0];
 const playersCurrentScore = [0, 0];
-let playingPlayer = 1;
+let turnPlayer = 0;
 
-const elCurrentScores = document.querySelectorAll('.current-score');
+const elPlayers = document.querySelectorAll(".player");
 
-const elScores = document.querySelectorAll('.score');
-const elScore0 = document.querySelector('#score--0');
-const elScore1 = document.querySelector('#score--1');
+const elCurrentScores = document.querySelectorAll(".current-score");
 
-const elDice = document.querySelector('.dice');
+const elScores = document.querySelectorAll(".score");
+const elScore0 = document.querySelector("#score--0");
+const elScore1 = document.querySelector("#score--1");
 
-const btns = document.querySelectorAll('.btn');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const elDice = document.querySelector(".dice");
 
-// C H A N G E  G A M E
+const btns = document.querySelectorAll(".btn");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
-// M O D I F Y  S T Y L E S
+// - C H A N G E  G A M E
 
-// L O G I C  G A M E
+// - M O D I F Y  S T Y L E S
+
+const changeStylePlayerActive = () => {
+    /*
+    let classList;
+
+    elPlayers[turnPlayer].classList += " player--active" + turnPlayer;
+
+    // classList = elPlayers[turnPlayer ? 0 : 1].classList.split(" ").length = 2;
+    let datos = [...(elPlayers[turnPlayer ? 0 : 1].classList)];
+
+    datos.length = 2;
+    elPlayers[turnPlayer ? 0 : 1].classList = datos.join(" ");
+
+    */
+    elPlayers[turnPlayer].classList.add(`player--active${turnPlayer ? "1" : "0"}`);
+    elPlayers[turnPlayer ? 0 : 1].classList.remove(`player--active${turnPlayer ? "0" : "1"}`);
+
+    // contains, toggle
+
+    // √ indicative--player (activar indicativo por jugador activo)
+};
+
+// √ (changeStylePlayerWinner) - mostrar jugador ganador [player--winner1, player--winner0] (174 & 179)
+
+// √ (winPlayer) - bloquer uso de botones
 
 
-const rollDice = function() {
+
+// - L O G I C  G A M E
+const playerChangeActive = function () {
+    turnPlayer = turnPlayer ? 0 : 1;
+    changeStylePlayerActive();
+};
+
+const rollDice = function () {
     let diceNumber = Math.floor(Math.random() * 6) + 1;
-    
-    // console.log("pepo sa cagao en el bote colacao");
-    
+
     if (elDice.style.display === "none") elDice.style.display = "block";
+
     elDice.setAttribute("src", `dice-${diceNumber}.png`);
+    // elDice.src = `dice-${diceNumber}.png`;
 
-    if (diceNumber !== 1) {
-        playersCurrentScore[playingPlayer - 1] += diceNumber;
-        elCurrentScores[playingPlayer - 1].textContent = playersCurrentScore[playingPlayer - 1];
+    if (diceNumber === 1) {
+        playersCurrentScore[turnPlayer] = 0;
+        elCurrentScores[turnPlayer].textContent = 0;
+        playerChangeActive();
     } else {
-        playersCurrentScore[playingPlayer - 1] = 0;
-        elCurrentScores[playingPlayer - 1].textContent = playersCurrentScore[playingPlayer - 1];
-
-
-        // add cambiar jugador
-        playingPlayer += 1;
-
-        if (playingPlayer > 2) {
-            playingPlayer = 1;
-        }
+        playersCurrentScore[turnPlayer] += diceNumber;
+        elCurrentScores[turnPlayer].textContent = playersCurrentScore[turnPlayer];
     }
-}
+};
 
-function comoTuQuieras(params) {
-    
-}
+const holdCurrent = function () {
+    playersScore[turnPlayer] += playersCurrentScore[turnPlayer];
+    playersCurrentScore[turnPlayer] = 0;
+    elCurrentScores[turnPlayer].textContent = "0";
+    elScores[turnPlayer].textContent = playersScore[turnPlayer];
 
-const holdCurrent = function() {
-    // console.log("pepo guardo sus puntos y fue rico y comió perdices");  
-    
-    playersScore[playingPlayer - 1] += playersCurrentScore[playingPlayer - 1]
-    playersCurrentScore[playingPlayer - 1] = 0;
-    elCurrentScores[playingPlayer - 1].textContent = "0";
-    elScores[playingPlayer - 1].textContent = playersScore[playingPlayer - 1]
+    playerChangeActive();
+};
 
-    
-    // add cambiar jugador
-    playingPlayer += 1;
-
-    if (playingPlayer > 2) {
-        playingPlayer = 1;
-    }
-}
-
-const resetGame = function() {
+const resetGame = function () {
     playersScore.forEach((playerScore, i) => {
-        playerScore = 0
-        
+        playerScore = 0;
+
         playersCurrentScore[i] = 0;
-        elCurrentScores[i].textContent = '0';
+        elCurrentScores[i].textContent = "0";
         elScores[i].textContent = "0";
     });
 
     elDice.style.display = "none";
-    
-    playingPlayer = Math.floor(Math.random() * 2) + 1;
+
+    turnPlayer = Math.floor(Math.random() * 2);
 
     btnRoll.addEventListener("click", rollDice);
     btnHold.addEventListener("click", holdCurrent);
 
-    // add cambiar jugador
-}
+    changeStylePlayerActive();
+};
 
+resetGame();
 
 btnNew.addEventListener("click", resetGame);
+
